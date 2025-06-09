21d5e60d23d3ed5798156055d33d55d22d35d65a
trojan9 <timmybanjo@gmail.com>
2025-06-09 13:11:28 +0100

    new fix

--- lib/src/behaviors/line_point_highlighter.dart
+++ lib/src/behaviors/line_point_highlighter.dart
@@ -22,6 +22,7 @@ import 'package:charts_common/common.dart' as common
         SelectionModelType,
         SymbolRenderer;
 import 'package:flutter/widgets.dart' show hashValues;
+
 import 'package:meta/meta.dart' show immutable;
 
 import 'chart_behavior.dart' show ChartBehavior, GestureType;
@@ -115,7 +116,7 @@ class LinePointHighlighter<D> extends ChartBehavior<D> {
 
   @override
   int get hashCode {
-    return hashValues(
+    return Object.hash(
       selectionModelType,
       defaultRadiusPx,
       radiusPaddingPx,
